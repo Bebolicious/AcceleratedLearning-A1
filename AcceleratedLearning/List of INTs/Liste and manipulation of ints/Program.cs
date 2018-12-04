b38705028004f53f4010d1678e5d60c7eba76fe5
Bebolicious <a.zetterberg@hotmail.com>
2018-12-04 14:22:04 +0100

    Sorting and manipulating lists in C#

---
+++ AcceleratedLearning/List of INTs/Liste and manipulation of ints/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Liste_and_manipulation_of_ints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* NOTE THIS APPLICATION IS NOT FINISHED
+             * NOTE THIS APPLICATION IS NOT FINISHED
+             * NOTE THIS APPLICATION IS NOT FINISHED
+             * NOTE THIS APPLICATION IS NOT FINISHED
+             * TODO - Take in ints in a list and make a total and median out of them*/
+
+
+
+
+            List<int> number = new List<int>();
+
+            do
+            {
+
+            
+                Console.Write("Enter a name: ");
+                int input = int.Parse(Console.ReadLine());     
+                number.Add(input);
+            }
+            while (int.Parse="quit");
+            { 
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine();
+                number.Sort();
+
+            }
+           
+            foreach (int resultat in number)
+            {
+                Console.WriteLine(resultat);
+            }
+
+
+        }
+    }
+}
---
+++ AcceleratedLearning/Sorting list/Sort and manipulate list/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort_and_manipulate_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Asking for names*/
+
+            
+            List<string> names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Enter a name: ");
+                string input = Console.ReadLine().Trim();
+                
+
+                /*Breaks the loop if the user enters "quit"*/
+                if (input.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+              
+              
+                /* Adding input to list*/
+                names.Add(input);
+
+            }
+            try
+            {
+                /* Sorting and printing out names*/
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine();
+                names.Sort();
+                names.RemoveAt(names.Count - 1);
+                names.RemoveAt(0);
+            }
+            catch (Exception)
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error while counting list objects");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            foreach (string resultat in names)
+            {
+                Console.WriteLine("Name: " + resultat);
+            }
+            
+
+        }
+    }
+}
