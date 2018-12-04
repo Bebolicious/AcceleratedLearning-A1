7d752468daf3123edde698bfeacac5c30651f088
Bebolicious <a.zetterberg@hotmail.com>
2018-12-04 13:05:07 +0100

    Added Watcher Event in C#

---
+++ AcceleratedLearning/System Watcher events/Systemevents/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Systemevents
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = @"c:\temp\Statistics.txt";
+            Console.WriteLine("You are monitoring the filepath ''C:/Temp'' \n\n");
+            File.CreateText(path);
+            var fil = new FileSystemWatcher();
+
+
+            fil.Path = @"C:\Temp";
+                fil.EnableRaisingEvents = true;
+                fil.Created += FileCreated;
+            fil.Changed += FileChanged;
+            fil.Deleted += FileDeleted;
+            fil.Renamed += FileRenamed;
+
+            Console.ReadKey();
+          
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"The file {e.Name} has been renamed");
+        }
+
+        static private void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"The file {e.Name} has been deleted");
+           
+        }
+
+       static private void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"The file {e.Name} has been changed");
+            
+        }
+
+        static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(e.Name + " Was created");
+            
+        }
+
+    }
+}
